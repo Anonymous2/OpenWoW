be373747be75451c3d65a58fc817673a3c0030cf
caffeine239 <caffeine239@yahoo.com>
2013-11-30 23:03:27 -0500

    Fixed PlayerLogin thanks Vincent Michael

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -728,23 +728,23 @@ void WorldSession::HandleCharDeleteOpcode(WorldPacket& recvData)
 {
 	ObjectGuid guid;
 	
-    guid[3] = recvData.ReadBit();
-    guid[4] = recvData.ReadBit();
-    guid[2] = recvData.ReadBit();
-	guid[5] = recvData.ReadBit();
-    guid[6] = recvData.ReadBit();
-    guid[1] = recvData.ReadBit();
-    guid[0] = recvData.ReadBit();
-    guid[7] = recvData.ReadBit();    
+    	guid[3] = recvData.ReadBit();
+    	guid[4] = recvData.ReadBit();
+    	guid[2] = recvData.ReadBit();
+    	guid[5] = recvData.ReadBit();
+    	guid[6] = recvData.ReadBit();
+    	guid[1] = recvData.ReadBit();
+    	guid[0] = recvData.ReadBit();
+    	guid[7] = recvData.ReadBit();    
 	
 	recvData.ReadByteSeq(guid[2]);
-    recvData.ReadByteSeq(guid[0]);
-    recvData.ReadByteSeq(guid[4]);
-    recvData.ReadByteSeq(guid[1]);
-    recvData.ReadByteSeq(guid[5]);
-    recvData.ReadByteSeq(guid[3]);
-    recvData.ReadByteSeq(guid[7]);
-    recvData.ReadByteSeq(guid[6]);	
+    	recvData.ReadByteSeq(guid[0]);
+    	recvData.ReadByteSeq(guid[4]);
+	recvData.ReadByteSeq(guid[1]);
+    	recvData.ReadByteSeq(guid[5]);
+	recvData.ReadByteSeq(guid[3]);
+    	recvData.ReadByteSeq(guid[7]);
+    	recvData.ReadByteSeq(guid[6]);	
 
     // can't delete loaded character
     if (ObjectAccessor::FindPlayer(guid))
@@ -826,14 +826,14 @@ void WorldSession::HandlePlayerLoginOpcode(WorldPacket& recvData)
 
     recvData >> unk;
 
-    playerGuid[1] = recvData.ReadBit();
-    playerGuid[0] = recvData.ReadBit();   
-    playerGuid[7] = recvData.ReadBit();
-    playerGuid[2] = recvData.ReadBit();
-    playerGuid[5] = recvData.ReadBit();
     playerGuid[6] = recvData.ReadBit();
+    playerGuid[7] = recvData.ReadBit();   
+    playerGuid[1] = recvData.ReadBit();
+    playerGuid[5] = recvData.ReadBit();
+    playerGuid[2] = recvData.ReadBit();
     playerGuid[4] = recvData.ReadBit();
-    playerGuid[3] = recvData.ReadBit();    
+    playerGuid[3] = recvData.ReadBit();
+    playerGuid[0] = recvData.ReadBit();    
 
     recvData.ReadByteSeq(playerGuid[7]);
     recvData.ReadByteSeq(playerGuid[6]);
